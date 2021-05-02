00e9fa6ead8781ac500c32e07831bcbaeb957111
pa-pro <patrycja.osuch@gmail.com>
2021-05-02 14:35:01 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log("Cześć!");
+
+let buttonRemove = document.querySelector(".js-buttonRemove");
+let body = document.querySelector(".body");
+let changeBackgroundButton = document.querySelector(".js-changeBackground");
+let changeHeaderButton = document.querySelector(".js-changeHeader");
+let section__header = document.querySelector(".section__header");
+let newHeader = document.querySelector(".newHeader");
+let newBackground = document.querySelector(".newBackground");
+let themeName1 = document.querySelector(".themeName1");
+let themeName2 = document.querySelector(".themeName2");
+
+buttonRemove.addEventListener("click", () => {
+    buttonRemove.remove();
+});
+
+changeBackgroundButton.addEventListener("click", () => {
+    body.classList.toggle("newBackground");
+
+    themeName1.innerText = body.classList.contains("newBackground") ? "Przywróć" : "Zmień";
+    
+});
+
+changeHeaderButton.addEventListener("click", () => {
+    section__header.classList.toggle("newHeader");
+
+    themeName2.innerText = section__header.classList.contains("newHeader") ? "Przywróć" : "Zmień";
+
+});
