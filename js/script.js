3553f3197d2f05b89071b79e7d0135edd7b795b8
pa-pro <patrycja.osuch@gmail.com>
2021-05-03 16:02:30 +0200

    js functions added

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    };
 
-let buttonRemove = document.querySelector(".js-buttonRemove");
-let body = document.querySelector(".body");
-let changeBackgroundButton = document.querySelector(".js-changeBackground");
-let changeHeaderButton = document.querySelector(".js-changeHeader");
-let section__header = document.querySelector(".section__header");
-let themeName1 = document.querySelector(".themeName1");
-let themeName2 = document.querySelector(".themeName2");
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const themeName1 = document.querySelector(".themeName1");
+        body.classList.toggle("newBackground");
+        themeName1.innerText = body.classList.contains("newBackground") ? "Przywróć" : "Zmień";
+    };
 
-buttonRemove.addEventListener("click", () => {
-    buttonRemove.remove();
-});
+    const toggleHeader = () => {
+        const themeName2 = document.querySelector(".themeName2");
+        const section__header = document.querySelector(".section__header");
+        section__header.classList.toggle("newHeader");
+        themeName2.innerText = section__header.classList.contains("newHeader") ? "Przywróć" : "Zmień";
+    };
 
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("newBackground");
+    const removeButton = () => {
+        const buttonRemove = document.querySelector(".js-buttonRemove");
+        buttonRemove.remove();
+    };
 
-    themeName1.innerText = body.classList.contains("newBackground") ? "Przywróć" : "Zmień";
-    
-});
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackground");
+        const changeHeaderButton = document.querySelector(".js-changeHeader");
+        const buttonRemove = document.querySelector(".js-buttonRemove");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+        changeHeaderButton.addEventListener("click", toggleHeader);
+        buttonRemove.addEventListener("click", removeButton);
+        
+        welcome();
+    };
 
-changeHeaderButton.addEventListener("click", () => {
-    section__header.classList.toggle("newHeader");
-
-    themeName2.innerText = section__header.classList.contains("newHeader") ? "Przywróć" : "Zmień";
-
-});
+    init();
+}
